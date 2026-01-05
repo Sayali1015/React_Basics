1c9abf62ce42a88edce46f1f13a85b8ee9ef389a
Sayali Rajwade <sayalirajwade077@gmail.com>
2026-01-05 16:45:17 +0530

    Modify Props

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 
-const Product = (props) => {
+const Product = ({title, color, brand, prize, ram, rom, camera, fingerprint}) => {
     
   return (
     <div>
-      <h2>Phone Name={props.title}</h2>
-      <h3>Phone color= {props.color}</h3>
-      <h4>Phone brand={props.brand}</h4>
-      <h5>Phone Prize={props.prize}</h5>
-      <h3>RAM = {props.ram}</h3>
-      <h3>ROM = {props.rom}</h3>
-      <h3>Camera = {props.camera}</h3>
-      <h1>fingerprint= {props.fingerprint}</h1>
+      <h2>Phone Name={title}</h2>
+      <h3>Phone color= {color}</h3>
+      <h4>Phone brand={brand}</h4>
+      <h5>Phone Prize={prize}</h5>
+      <h3>RAM = {ram}</h3>
+      <h3>ROM = {rom}</h3>
+      <h3>Camera = {camera}</h3>
+      <h1>fingerprint= {fingerprint}</h1>
     </div>
   )
 }
