7eab38fcf4d1c6712ad0c2dc124c038ad72b3557
Sayali Rajwade <sayalirajwade077@gmail.com>
2026-01-05 17:06:06 +0530

    Added Component with props

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,10 @@ import Person from './components/Person'
 import Strangerthings from './components/Strangerthings'
 import { Test} from './components/Test'
 import Product from './components/Product'
+import Person2 from './components/Person2'
+
+
 const App = () => {
-  
   return (
     <>
     {/* <h1>App is an entry point</h1> */}
@@ -12,15 +14,24 @@ const App = () => {
     {/* <Strangerthings/> */}
     {/* <Test/> */}
 
+
+<div>
+  
     <Product title = "Galaxy S24 Ultra" color = "black" brand = "Samsung" 
     prize ={150000} ram = "8GB" rom = "128 GB" camera = "200 MP"/>
 
     <Product title = "Iphone 16 Pro" color = "Golden" brand = "Apple" 
     prize ={250000} fingerprint = "Yes"/>
   
-    <Product title = "One Plus" color = "White" brand = "OnePlus" 
-    prize ={34000}/>
+    <Product title = "One Plus" color = "White" brand = "OnePlus" prize ={34000}/> 
+    </div>
 
+
+<div>
+    <Person2 name= "Sayali Rajwade" MobileNumber= {9021628100} Gmail= "sayalirajwade077@gmail.com" pincode = {411017}/>
+    <Person2 name= "Kunal Rajwade" MobileNumber= {7498853712} Gmail= "kunalrajwade09@gmail.com" pincode = {411018}/>
+    <Person2 name= "Om Gaikwad" MobileNumber= {9763121262} Gmail= "omgaikwad12@gmail.com" pincode = {411003} ram = "sita"/>
+</div>
     </>
   )
 };
---
+++ src/components/Person2.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Person2 = ({name, MobileNumber, Gmail, pincode, ram}) => {
+  return (
+    <div>
+      <h3>Name = {name}</h3>
+      <h3>MobileNumber = {MobileNumber}</h3>
+      <h3>Gmail = {Gmail}</h3>
+      <h3>Pincode = {pincode}</h3>
+      <h1>Ram = {ram}</h1>
+
+    </div>
+  )
+}
+
+export default Person2
