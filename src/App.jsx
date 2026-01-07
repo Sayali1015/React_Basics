f36a93d1825dccec31662034b17a9e1fb8a57b44
Sayali Rajwade <sayalirajwade077@gmail.com>
2026-01-07 14:58:51 +0530

    Added Filter

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Laptop from './components/Laptop'
 import Events from './components/Events'
 import Counter from './components/Counter'
 import Showproducts from './components/MapProducts'
+import FilterProduct from './components/FilterProduct'
 
 const App = () => {
   return (
@@ -54,8 +55,12 @@ const App = () => {
       <Counter/>
     </div> */}
 
-    <div>
+    {/* <div>
       <Showproducts/>
+    </div> */}
+
+    <div>
+      <FilterProduct/>
     </div>
 
 
---
+++ src/components/FilterProduct.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const FilterProduct = () => {
+    const product =[
+        {id:1, title: 'iphone', category: 'Mobile', price: 90000},
+        {id:2, title: 'Asus', category: 'Laptop', price: 100000},
+        {id:3, title: 'Mi', category: 'Tablet', price: 20000},
+        {id:4, title: 'Boat', category: 'Speaker', price: 30000},
+         {id:5, title:'HP', category: 'Laptop', price: 480000}
+
+    ]
+    const filtereddata = product.filter((data)=>data.category == 'Laptop')
+    console.log(filtereddata);
+  return (
+    <div>
+      {filtereddata.map((data)=><div key={data.id}>
+        <h1>{data.title}</h1>
+        <h1>{data.price}</h1>
+        <h1>{data.category}</h1>
+      </div>)}
+    </div>
+  )
+}
+
+export default FilterProduct
