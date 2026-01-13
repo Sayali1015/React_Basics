bc03eff81498cfc09c50b90cf48082ff5e6a2425
Sayali Rajwade <sayalirajwade077@gmail.com>
2026-01-13 13:43:48 +0530

    Added input button and api for search wise dishes

--- src/Food_Recipe/Meal.jsx
+++ src/Food_Recipe/Meal.jsx
@@ -1,63 +1,96 @@
 import React, { useEffect, useState } from 'react'
+import './Meal.css'
 
 const Meal = () => {
   const [mealData, setMealData] = useState([]);
+  const [area, setArea] = useState('Indian');
+  const [inputData, setInputData] = useState('');
+
+
 
   useEffect(() => {
-    const fetchDataFromAPI = async () =>{
-      const api = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?a=Canadian`);
+  if (!area) return;
+
+  const fetchDataFromAPI = async () => {
+    const api = await fetch(
+      `https://www.themealdb.com/api/json/v1/1/filter.php?a=${area}`
+    );
+    const data = await api.json();
+    setMealData(data.meals);
+  };
+
+  fetchDataFromAPI();
+}, [area]);
+
+
+  const submitHandler = async (e)=>{
+    e.preventDefault();
+    setArea("");
+     const api = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${inputData}`);
 
       const data = await api.json();
 
-      console.log(data.meals);
+      console.log('Search data =',data.meals);
       setMealData(data.meals);
-    };
-    fetchDataFromAPI()
-  }, [])
+      // setInputData (' ')
+  }
   
   return (
     <>
     <div className='mx-auto text-center'>
+      
         <button 
-        // onClick={()=>setMovieList(movies)} 
+        onClick={()=>setArea('Indian')} 
         type="button" 
-        className="btn btn-outline-primary mx-3"
+        className="btn btn-outline-warning mx-3"
         >
         Indian</button>
 
         <button 
-        // onClick={()=>filterByCategory("Action")} 
+        onClick={()=>setArea('Canadian')} 
         type="button" 
-        className="btn btn-outline-danger mx-3">
+        className="btn btn-outline-info mx-3">
         Canadian</button>
 
         <button 
-        // onClick={()=>filterByCategory("Thriller")} 
+        onClick={()=>setArea('American')} 
         type="button" 
         className="btn btn-outline-light mx-3">
         American</button>
 
         <button 
-        // onClick={()=>filterByCategory("Animation")} 
+        onClick={()=>setArea('Thai')} 
         type="button" 
         className="btn btn-outline-info mx-3">
         Thai</button>
 
         <button 
-        // onClick={()=>filterByCategory("Horror")} 
+        onClick={()=>setArea('British')} 
         type="button" 
         className="btn btn-outline-warning mx-3">
         British</button>
 
         <button 
-        // onClick={()=>filterByCategory("Drama")} 
+       onClick={()=>setArea('Russian')}  
         type="button" 
         className="btn btn-outline-info mx-3">
         Russian</button>
 
+      
+      
+        <form onSubmit={submitHandler}>
+        <input
+          className="mx-auto my-4"
+          onChange={(e) => setInputData(e.target.value)}
+          placeholder="Search"
+          type="text"
+        />
+      </form>
+
       </div>
 
-    <div style={{display:'flex', justifyContent: 'center', alignContent:'center', flexWrap:'wrapg'}}>
+
+    <div style={{display:'flex', justifyContent: 'center', alignContent:'center', flexWrap:'wrap'}}>
       {mealData.map((data)=><div key = {data.idMeal} style={{textAlign: 'center'}}>
         <div>
           <img src={data.strMealThumb} alt="" style={{width:'220px', borderRadius:'10px', border: '2px solid blue'}} />
