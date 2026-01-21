f13d7d46de036a24daae64784b81b7cedba9b957
Sayali Rajwade <sayalirajwade077@gmail.com>
2026-01-21 14:02:19 +0530

    added Dynamic Navigation bar Creation and Condition Rendering

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,43 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link , useNavigate} from 'react-router-dom'
 import './Navbar.css'
 
 const Navbar = () => {
-    const [isLogin, setIsLogin] = useState(false)
+    const [isLogin, setIsLogin] = useState(false);
+    const navigate = useNavigate()
+    const login = ()=>{
+        setIsLogin(true)
+        navigate('/dashboard')
+    }
+
+    const logout = ()=>{
+        setIsLogin(false);
+    }
 
 
   return (
     <div className='nav_bar'>
       <Link  to={'/'} className='left items'>WDM</Link>
       <div className='right'>
+
+    {isLogin && (
+        <>
         <Link className= "items" to={'/meal'}>Meal</Link>
         <Link className= "items" to={'/courses'}>Courses</Link>
         <Link className= "items" to={'/dashboard'}>Dashboard</Link>
         <Link className= "items" to={'/profile'}>Profile</Link>
+        <Link className= "items" style={{fontWeight: 'bold', backgroundColor: 'grey', margin: '10px'}} onClick={logout}>Logout</Link>
+       </>)}
+
+       {!isLogin && (<>
+      
+
         <Link className= "items" to={'/about'}>About</Link>
         <Link className= "items" to={'/team'}>Team</Link>
         <Link className= "items" to={'/contact'}>Contact</Link>
-        <Link className= "items" style={{fontWeight: 'bold', backgroundColor: 'grey', margin: '10px'}}>Logout</Link>
-        <Link className= "items" style={{fontWeight: 'bold', backgroundColor: 'grey',  margin:"10px"}}>Login</Link>
-
+        {/* <Link className= "items" style={{fontWeight: 'bold', backgroundColor: 'grey', margin: '10px'}}>Logout</Link> */}
+        <button className= "items" style={{fontWeight: 'bold', backgroundColor: 'grey',  margin:"10px"}} onClick={login} >Login</button>
+        </> )}
 
       </div>
     </div>
