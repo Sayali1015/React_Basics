dab649028b3ad08dbd1705a124fac576ed12d2a8
Sayali Rajwade <sayalirajwade077@gmail.com>
2026-01-21 13:04:50 +0530

    added ReactHook (useNavigate)

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const About = () => {
+  const navigate = useNavigate();
+  const goToHome = () =>{
+    navigate('/');
+  }
+
+  const goBack = ()=>{
+    navigate(-1);
+  }
   return (
     <div>
-      aboutwalapage
+      <button onClick={goToHome}> Go To Home</button>
+      <button onClick={goBack}> Go Back</button>
     </div>
   )
 }
